4016b8fa3b05eed824284a494293f35378243093
Saurabh Singh <saurabhsingh1@ternaengg.ac.in>
2020-06-28 17:42:02 +0530

    Added mod function

--- main.py
+++ main.py
@@ -3,5 +3,16 @@
 Created at 28-06-2020
 '''
 
+def mod(x : int, y : int) -> int:
+    ''' 
+    @params 
+    x : int
+    y : int
+
+    @return
+    x % y
+    '''
+    return x % y
+
 if __name__ == '__main__':
     print("V.1")
